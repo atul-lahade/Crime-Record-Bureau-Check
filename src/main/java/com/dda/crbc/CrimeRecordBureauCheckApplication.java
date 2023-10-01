3e7ffa1a8785aec4148febe4c1a3854e1053a184
AtulLahade <atul.lahade@mendix.com>
2023-10-01 15:33:07 +0530

    Initial commit

---
+++ src/main/java/com/dda/crbc/CrimeRecordBureauCheckApplication.java
@@ -0,0 +1,13 @@
+package com.dda.crbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeRecordBureauCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimeRecordBureauCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dda/crbc/CrimeRecordBureauCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dda.crbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeRecordBureauCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
