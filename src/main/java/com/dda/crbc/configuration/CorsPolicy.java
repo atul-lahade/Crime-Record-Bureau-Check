f40aad33a016e172ccb979f1b48ad4aa25640cfa
AtulLahade <atul.lahade@mendix.com>
2023-10-13 15:10:11 +0530

    Added CORS and application.yml

---
+++ src/main/java/com/dda/crbc/configuration/CorsPolicy.java
@@ -0,0 +1,36 @@
+package com.dda.crbc.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+/**
+ * CORS configuration for all environments.
+ */
+@Slf4j
+@Configuration
+public class CorsPolicy {
+
+    /**
+     * CORS configuration for local, all origins and methods are allowed.
+     * @return
+     */
+    @Profile({"local"})
+    @Bean(name = "corsConfigurationSource")
+    CorsConfigurationSource corsConfigurationSourceLocalAndDev() {
+        log.info("Local/Dev CORS policy applied");
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
