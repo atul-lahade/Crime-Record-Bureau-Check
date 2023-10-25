c7bffae0830c6f7a326fd80764efe4291ff02387
AtulLahade <atul.lahade@mendix.com>
2023-10-25 15:33:01 +0530

    Added enum for user type

---
+++ src/main/java/com/dda/crbc/constants/UserType.java
@@ -0,0 +1,31 @@
+package com.dda.crbc.constants;
+
+public enum UserType {
+
+    ADMINISTRATOR("Admin"),
+    APPLICANT("Applicant");
+
+    private final String value;
+
+    UserType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static UserType of(String value) {
+        for (UserType userType : UserType.values()) {
+            if (userType.getValue().equals(value)) {
+                return userType;
+            }
+        }
+        throw new IllegalArgumentException(value + " is not a valid user type");
+    }
+
+    @Override
+    public String toString() {
+        return this.getValue();
+    }
+}
